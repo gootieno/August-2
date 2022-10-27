a93b11486b5af732fc855043cb611a9a3dad36b0
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2022-10-27 14:36:11 -0700

    local storage notes

--- Week9/Day4/solutions/catsagram/public/storage.js
+++ Week9/Day4/solutions/catsagram/public/storage.js
@@ -2,6 +2,10 @@
 export const buildStorage = () => {
   const { catImage, upVote, downVote, comments } = localStorage;
 
+  // this is evaluating the local storage items above and acting like an inline if else statement
+  // catImage example: if we have a catImage in local storage then set it. Otherwise create the key in 
+  // local storage but set the value as an empty string. If any more storage properties are needed then 
+  // you can add them using the following syntax below. Note that any data structures must be stringified and parsed during retrieval
   localStorage.setItem("catImage", catImage ? catImage : "");
   localStorage.setItem("upVote", upVote ? upVote : 0);
   localStorage.setItem("downVote", downVote ? downVote : 0);
